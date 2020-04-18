d9cdf02ba7be77c79d4674a89f3d6f4bce551c98
Matias Cabre <matiascabre94@gmail.com>
2020-04-18 19:12:17 -0300

    problema triangulo

---
+++ triangulos/src/com/company/Main.java
@@ -0,0 +1,48 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        int lado1,lado2,lado3;
+
+        System.out.println("Ingrese la longitud del primer lado: ");
+        lado1 = scanner.nextInt();
+
+        System.out.println("Ingrese la longitud del segundo lado: ");
+        lado2 = scanner.nextInt();
+
+        System.out.println("Ingrese la longitud del tercer lado: ");
+        lado3 = scanner.nextInt();
+
+        if(!esValidoTriangulo(lado1,lado2,lado3)){
+            System.out.println("El triángulo no es válido.\nLa suma de dos lados no puede ser menor o igual al lado restante.");
+            return;
+        }
+
+        if(lado1 == lado2 && lado1 == lado3){
+            System.out.println("El triángulo es equilátero");
+            return;
+        }
+        
+        if(lado1 == lado2 || lado1 == lado3 || lado2 == lado3){
+            System.out.println("El triángulo es isóceles");
+            return;
+        }
+
+        System.out.println("El triángulo es escaleno");
+        return;
+
+    }
+
+    private static boolean esValidoTriangulo(int lado1, int lado2, int lado3) {
+        if(lado1 + lado2 <= lado3 || lado1 + lado3 <= lado2 || lado2 + lado3 <= lado1){
+            return false;
+        }else{
+            return true;
+        }
+    }
+}
