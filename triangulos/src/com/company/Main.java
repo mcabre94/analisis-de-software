9435cfeda1174190fc33873925212e219b1fc6ee
Matias Cabre <matiascabre94@gmail.com>
2020-04-24 19:25:06 -0300

    no se arroja excepcion cuando el lado es decimal

--- triangulos/src/com/company/Main.java
+++ triangulos/src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -9,14 +10,20 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int lado1,lado2,lado3;
 
-        System.out.println("Ingrese la longitud del primer lado: ");
-        lado1 = scanner.nextInt();
+        try{
+            System.out.println("Ingrese la longitud del primer lado: ");
+            lado1 = scanner.nextInt();
 
-        System.out.println("Ingrese la longitud del segundo lado: ");
-        lado2 = scanner.nextInt();
+            System.out.println("Ingrese la longitud del segundo lado: ");
+            lado2 = scanner.nextInt();
 
-        System.out.println("Ingrese la longitud del tercer lado: ");
-        lado3 = scanner.nextInt();
+            System.out.println("Ingrese la longitud del tercer lado: ");
+            lado3 = scanner.nextInt();
+        }catch (InputMismatchException exception){
+            System.out.println("Error : La longitud del lado debe ser entera");
+            return;
+        }
+
 
         if(!esValidoTriangulo(lado1,lado2,lado3)){
             System.out.println("El triángulo no es válido.\nLa suma de dos lados no puede ser menor o igual al lado restante.");
